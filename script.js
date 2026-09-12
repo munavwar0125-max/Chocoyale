dab1dbc8f1281d2caca8fe089b7483450e23a7b2
Munavwar Shaik <munavwar0125@gmail.com>
2026-09-12 20:20:11 +0530

    update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 
 const track = document.getElementById('track');
 let index = 0;
-
+const ts=3;
 setInterval(() => {
-  index = (index + 1) % 3;
-  track.style.transform = `translateX(-${index * 33.333}%)`;
+  index = (index + 1) % ts;
+  track.style.transform = `translateX(-${index * 100/ts}%)`;
 }, 3000); // change every 3 seconds
