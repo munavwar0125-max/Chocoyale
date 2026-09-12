00adbbbf18db05060a86ab354396faaf2f105131
Munavwar Shaik <munavwar0125@gmail.com>
2026-09-12 20:44:43 +0530

    update script.js

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 
+const t=documemt.getElementsByClass("track");
+const slides=t.child;
+const tot=slides.length;
+setTimeout(()=>{
+  i=(i+1)%tot;
+  t.style.transform=`translateX(-${i*(100/tot)}%)`;
+},3000);
 
-const track = document.getElementById('track');
+
+
+
+
+
+
+
+
+
+
+
+
+/*const track = document.getElementById('track');
 let index = 0;
 const ts=3;
 setInterval(() => {
   index = (index + 1) % ts;
   track.style.transform = `translateX(-${index * 100/ts}%)`;
-}, 5000); // change every 3 seconds
+}, 5000); *///change every 3 seconds//
